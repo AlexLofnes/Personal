f73b36d2a326743ed1cb64f0007081c981525fd3
Alex Lofnes <alexlofnes@gmail.com>
2021-12-24 12:09:18 +0000

    swapped radix for shell

--- src/slow.svelte
+++ src/slow.svelte
@@ -96,70 +96,26 @@
         completed = true;
     }
 
-    async function radixBucketSort () {
+    async function shellSort() {
         d = new Date();
         startTime = d.getTime()/1000;
-        
-        var idx1, idx2, idx3, len1, len2, radix, radixKey;
-        var radices = {}, buckets = {}, num, curr;
-        var currLen, radixStr, currBucket;
 
-        len1 = arr.length;
-        len2 = 10;  // radix sort uses ten buckets
+        let n = arr.length;
 
-        // find the relevant radices to process for efficiency        
-        for (idx1 = 0;idx1 < len1;idx1++) {
-        radices[arr[idx1].toString().length] = 0;
-        }
+        for (let gap = Math.floor(n/2); gap > 0; gap = Math.floor(gap/2))	{
+            for (let i = gap; i < n; i += 1)  {
+                let temp = arr[i];
+                
+                let j;
+                for (j = i; j >= gap && arr[j-gap] > temp; j-=gap)  {
+                    arr[j] = arr[j-gap];
+                }
 
-        // loop for each radix. For each radix we put all the arr
-        // in buckets, and then pull them out of the buckets.
-        for (radix in radices) {          
-        // put each array item in a bucket based on its radix value
-        len1 = arr.length;
-        for (idx1 = 0;idx1 < len1;idx1++) {
-            curr = arr[idx1];
-            // item length is used to find its current radix value
-            currLen = curr.toString().length;
-            // only put the item in a radix bucket if the item
-            // key is as long as the radix
-            if (currLen >= radix) {
-            // radix starts from beginning of key, so need to
-            // adjust to get redix values from start of stringified key
-            radixKey = curr.toString()[currLen - radix];
-            // create the bucket if it does not already exist
-            if (!buckets.hasOwnProperty(radixKey)) {
-                buckets[radixKey] = [];
-            }
-            // put the array value in the bucket
-            buckets[radixKey].push(curr);         
-            } else {
-            if (!buckets.hasOwnProperty('0')) {
-                buckets['0'] = [];
-            }
-            buckets['0'].push(curr);
-            }
-        }
-        // for current radix, arr are in buckets, now put them
-        // back in the array based on their buckets
-        // this index moves us through the array as we insert arr
-        idx1 = 0;
-        // go through all the buckets
-        for (idx2 = 0;idx2 < len2;idx2++) {
-            // only process buckets with arr
-            if (buckets[idx2] != null) {
-            currBucket = buckets[idx2];
-            // insert all bucket arr into array
-            len1 = currBucket.length;
-            for (idx3 = 0;idx3 < len1;idx3++) {
-                arr[idx1++] = currBucket[idx3];
+                arr[j] = temp;
                 await new Promise((r) => setTimeout(r, 1));
             }
-            }
         }
-        buckets = {};
-        }
-    d = new Date();
+        d = new Date();
         endTime = d.getTime()/1000;
         timeTaken = endTime-startTime;
         completed = true;
@@ -271,7 +227,7 @@
     <button on:click={insertionSort}>Insertion Sort</button>
     <button on:click={gnomeSort}>Gnome Sort</button>
     <button on:click={selectionSort}>Selection Sort</button>
-    <button on:click={radixBucketSort}>Radix Bucket Sort</button>
+    <button on:click={shellSort}>Shell Sort</button>
     <button on:click={flashSort}>Flash Sort</button>
 </main>
 <br>
